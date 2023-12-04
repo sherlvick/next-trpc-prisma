adcbb88eae12197a800a244b01942d71868ff9c7
sherlvick <kvicky047@gmail.com>
2023-12-05 01:50:57 +0530

    [BOOTSTARP]

---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "#32288A",
+        grey: "#F6F6F7",
+        pink: "#9C16A8",
+      },
+      height: {
+        "93vh": "93.5vh",
+        "45vh": "45vh",
+      },
+    },
+  },
+  plugins: [],
+};
